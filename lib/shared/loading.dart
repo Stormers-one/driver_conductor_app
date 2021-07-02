a698488a3d58d493006dbbe87130f8c8612167ab
Kennith Philips <35496496+kenn850@users.noreply.github.com>
2021-07-02 22:39:46 +0530

    loading color change

--- lib/shared/loading.dart
+++ lib/shared/loading.dart
@@ -6,7 +6,7 @@ class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.orange[300],
+      color: Colors.orange[100],
       child: Center(
         child: SpinKitFadingFour(
           color: red,
